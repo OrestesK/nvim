e3d72391c93bd563465b41e506610d552aed9390
OrestesK <oresteskassian@gmail.com>
2025-05-07 16:01:05 -0400

    change: noice -> notify only

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,30 +1,17 @@
 return {
-  -- UI + Notifications
+  -- Notifications
   {
-    'folke/noice.nvim',
-    event = 'VeryLazy',
-    opts = {
-      lsp = {
-        override = {
-          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-          ['vim.lsp.util.stylize_markdown'] = true,
-        },
-        signature = {
-          enabled = false,
-        },
-      },
-      presets = {
-        bottom_search = true, -- use a classic bottom cmdline for search
-        command_palette = true, -- position the cmdline and popupmenu together
-        long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false, -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = true, -- add a border to hover docs and signature help
-      },
-    },
-    dependencies = {
-      'MunifTanjim/nui.nvim',
-      'rcarriga/nvim-notify',
-    },
+    'rcarriga/nvim-notify',
+    config = function()
+      require('notify').setup {
+        level = 2,
+        timeout = 1000,
+        top_down = false,
+        render = 'wrapped-compact',
+        stages = 'fade',
+        fps = 60,
+      }
+    end,
   },
 
   -- HEX colors
