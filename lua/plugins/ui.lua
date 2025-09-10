406dbe5e3bc93c0c3614a34dfa92a824d9f939f7
OrestesK <oresteskassian@gmail.com>
2025-09-10 01:51:33 -0400

    change: word highlights

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -10,6 +10,7 @@ return {
         render = 'wrapped-compact',
         stages = 'fade',
         fps = 60,
+        merge_duplicates = false,
       }
     end,
   },
@@ -31,23 +32,68 @@ return {
     end,
   },
 
-  -- Cursor animation
-  -- {
-  --   'sphamba/smear-cursor.nvim',
-  --   opts = {
-  -- smear_insert_mode = false,
-  -- smear_between_buffers = false,
-  -- smear_vertically = false,
-  -- smear_diagonally = false,
-  -- smear_to_cmd = false,
-  --
-  -- stiffness = 0.8,
-  -- trailing_stiffness = 0.5,
-  -- distance_stop_animating = 0.5,
-  -- never_draw_over_target = false,
-  --   },
-  -- },
+  -- Highlight words
+  {
+    'lfv89/vim-interestingwords',
+    keys = {
+      {
+        '<leader>k',
+        mode = { 'n' },
+        ":call InterestingWords('n')",
+        desc = 'Highlight Word',
+        silent = true,
+      },
+      {
+        '<leader>k',
+        mode = { 'v' },
+        ":call InterestingWords('v')",
+        desc = 'Highlight Word',
+        silent = true,
+      },
+      {
+        '<leader>K',
+        mode = { 'n' },
+        ':call UncolorAllWords()',
+        desc = 'Clear Highlighted Words',
+        silent = true,
+      },
+    },
+  },
 
+  {
+    'tzachar/local-highlight.nvim',
+    config = function()
+      require('local-highlight').setup {
+        insert_mode = false,
+        min_match_len = 1,
+        max_match_len = 100,
+        highlight_single_match = true,
+        animate = {
+          enabled = false,
+        },
+        debounce_timeout = 0,
+      }
+    end,
+  },
+
+  -- Cursor animation
+  {
+    'sphamba/smear-cursor.nvim',
+    opts = {
+      smear_insert_mode = false,
+      smear_between_buffers = false,
+      smear_vertically = false,
+      smear_diagonally = false,
+      smear_to_cmd = false,
+
+      stiffness = 0.8,
+      trailing_stiffness = 0.5,
+      distance_stop_animating = 0.5,
+      never_draw_over_target = false,
+    },
+  },
+
+  -- Search and Replace
   { 'nvim-pack/nvim-spectre' },
 
   -- Aerial (lsp symbol navigation)
