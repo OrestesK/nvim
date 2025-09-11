497ce720f02db1734b3bd604acc032a5f11cc260
OrestesK <oresteskassian@gmail.com>
2025-09-11 18:38:28 -0400

    feat: nvim-lint and mason integration

---
+++ lua/core/lint.lua
@@ -0,0 +1,33 @@
+-- Lint
+return {
+  -- Linter
+  {
+    'mfussenegger/nvim-lint',
+    event = { 'BufReadPre', 'BufNewFile' },
+    config = function()
+      local lint = require 'lint'
+      lint.linters_by_ft = {
+        markdown = { 'markdownlint-cli2' },
+        python = { 'ruff' },
+        javascript = { 'quick-lint-js' },
+        typescript = { 'quick-lint-js' },
+      }
+      local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+        group = lint_augroup,
+        callback = function()
+          -- Only run the linter in buffers that you can modify in order to
+          -- avoid superfluous noise, notably within the handy LSP pop-ups that
+          -- describe the hovered symbol using Markdown.
+          if vim.bo.modifiable then
+            lint.try_lint()
+          end
+        end,
+      })
+    end,
+  },
+  -- Use Mason linters
+  {
+    'rshkarin/mason-nvim-lint',
+  },
+}
